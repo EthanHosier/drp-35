a2bb053752cc079b469990fffccb3fc2f3a8e518
Francois Poh <fp522@ic.ac.uk>
2024-05-26 15:10:50 +0100

    feat: add project pages skeleton

---
+++ app/(authenticated)/ongoing-projects/add-project.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from "react-native";
+
+const AddProject = () => {
+  return (
+      <View>
+        <Text>
+          add project
+        </Text>
+      </View>
+  );
+};
+
+export default AddProject;
---
+++ app/(authenticated)/ongoing-projects/projects.tsx
@@ -0,0 +1,16 @@
+import {Text, TouchableOpacity, View} from "react-native";
+import React from "react";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {defaultStyles} from "@/constants/DefaultStyles";
+import Colors from "@/constants/Colors";
+import {Link} from "expo-router";
+
+const OngoingProjects = () => {
+  return (
+      <View>
+        <Text>projects</Text>
+      </View>
+  );
+};
+
+export default OngoingProjects;
