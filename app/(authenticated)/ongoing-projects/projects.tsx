c4fbbeaf1c9f77b26d92d6cc2806f2b36ebc6155
Francois Poh <fp522@ic.ac.uk>
2024-05-26 15:11:38 +0100

    feat: add navigation buttons

--- app/(authenticated)/ongoing-projects/projects.tsx
+++ app/(authenticated)/ongoing-projects/projects.tsx
@@ -7,10 +7,38 @@ import {Link} from "expo-router";
 
 const OngoingProjects = () => {
   return (
-      <View>
-        <Text>projects</Text>
-      </View>
+      <SafeAreaView>
+        <View style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: 20,
+          marginBottom: 60,
+          paddingHorizontal: 20,
+        }}>
+          <LinkButton text="Add Project" href="./add-project" />
+          <LinkButton text="Join Project" href="./join-project" />
+        </View>
+      </SafeAreaView>
   );
 };
+
+const LinkButton = ({text, href}: {text: string, href: string}) => {
+  return (
+      <Link
+        href={href}
+        style={[
+          defaultStyles.pillButton,
+          {flex: 1, backgroundColor: Colors.primary},
+        ]}
+        asChild
+      >
+        <TouchableOpacity>
+          <Text style={{color: "white", fontSize: 15, fontWeight: "500"}}>
+            {text}
+          </Text>
+        </TouchableOpacity>
+      </Link>
+  );
+}
 
 export default OngoingProjects;
