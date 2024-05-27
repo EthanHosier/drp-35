a44c7ff8ad6093034c94fc45f1f86157a9420a5c
Ethan Hosier <ethanjhosier@gmail.com>
2024-05-27 11:48:55 +0100

    style: add ui to projects page

--- app/(authenticated)/ongoing-projects/projects.tsx
+++ app/(authenticated)/ongoing-projects/projects.tsx
@@ -1,54 +1,54 @@
-import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
-import {defaultStyles} from "@/constants/DefaultStyles";
+import { defaultStyles } from "@/constants/DefaultStyles";
 import Colors from "@/constants/Colors";
-import {Link} from "expo-router";
-import ProjectPreview from "@/components/projects/project-preview";
+import { Link } from "expo-router";
+import ProjectPreview from "@/components/projects/_project-preview";
 
-const placeholderProjects = Array.from(
-    {length: 20},
-    (_, i) => i
-);
+const placeholderProjects = Array.from({ length: 20 }, (_, i) => i);
 
 const OngoingProjects = () => {
   return (
-      <View style={styles.main}>
-        <Text style={styles.title}>Projects you've joined</Text>
-        <ScrollView contentContainerStyle={styles.projects}>
-          {placeholderProjects.map((i) => (
-              <ProjectPreview
-                  name={'Example Project' + i}
-                  description={`This is project number ${i}'s description.`}
-                  key={i}
-              />
-          ))}
-        </ScrollView>
-        <View style={styles.buttons}>
-          <LinkButton text="Add Project" href="add-project" />
-          <LinkButton text="Join Project" href="join-project" />
-        </View>
+    <View style={styles.main}>
+      <Text style={styles.title}>Projects you've joined</Text>
+      <ScrollView contentContainerStyle={styles.projects}>
+        {placeholderProjects.map((i) => (
+          <ProjectPreview
+            name={"Example Project" + i}
+            description={`This is project number ${i}'s description.`}
+            key={i}
+          />
+        ))}
+      </ScrollView>
+      <View style={styles.buttons}>
+        <LinkButton text="Add Project" href="add-project" />
+        <LinkButton text="Join Project" href="join-project" />
       </View>
+    </View>
   );
 };
 
-const LinkButton = ({text, href}: {text: string, href: string}) => {
+const LinkButton = ({ text, href }: { text: string; href: string }) => {
   return (
-      <Link
-        href={'/(authenticated)/ongoing-projects/' + href}
-        style={[
-          defaultStyles.pillButton,
-          styles.button,
-        ]}
-        asChild
-      >
-        <TouchableOpacity>
-          <Text style={{color: "white", fontSize: 15, fontWeight: "500"}}>
-            {text}
-          </Text>
-        </TouchableOpacity>
-      </Link>
+    <Link
+      href={"/(authenticated)/ongoing-projects/" + href}
+      style={[defaultStyles.pillButton, styles.button]}
+      asChild
+    >
+      <TouchableOpacity>
+        <Text style={{ color: "white", fontSize: 15, fontWeight: "500" }}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    </Link>
   );
-}
+};
 
 export default OngoingProjects;
 
@@ -63,14 +63,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     marginTop: 20,
   },
-  projects:{
+  projects: {
     flex: 1,
     alignItems: "center",
     marginBottom: 20,
   },
   button: {
     flex: 1,
-    backgroundColor: Colors.primary
+    backgroundColor: Colors.primary,
   },
   buttons: {
     flexDirection: "row",
---
+++ components/projects/_project-preview.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import Colors from "@/constants/Colors";
+
+const ProjectPreview = ({
+  name,
+  description,
+}: {
+  name: string;
+  description: string;
+}) => {
+  return (
+    <TouchableOpacity style={styles.preview}>
+      <Text style={{ color: "white", fontSize: 22, fontWeight: "500" }}>
+        {name}
+      </Text>
+      <Text style={{ color: "white", fontSize: 10, fontWeight: "500" }}>
+        {description}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+export default ProjectPreview;
+
+const styles = StyleSheet.create({
+  preview: {
+    backgroundColor: Colors.primaryMuted,
+    margin: 1,
+    borderRadius: 10,
+    height: 60,
+    width: 500,
+    maxWidth: "100%",
+    paddingHorizontal: 10,
+  },
+});
