85b557177fb915a3f4c2b39f506d7525583b47ca
Francois Poh <fp522@ic.ac.uk>
2024-05-28 12:57:21 +0100

    feat: support link redirection

--- components/profile/profile-card.tsx
+++ components/profile/profile-card.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View, Text} from "react-native";
+import {ScrollView, StyleSheet, View, Text, Linking} from "react-native";
 import {Image} from "expo-image";
 import Colors from "@/constants/Colors";
 
@@ -24,6 +24,28 @@ const EXAMPLE_DETAILS = [
 ]
 
 const ProfileCard = () => {
+
+  const handleLinkPress = (url: string) => {
+    Linking.openURL(url).catch((err) => console.error("Failed to open URL:", err));
+  };
+
+  const LinkText = ({ text }: { text: string }) => {
+    const regex = /(https?:\/\/\S+)/g;
+    const parts = text.split(regex);
+
+    return parts.map((part, index) =>
+      regex.test(part)
+        ? <Text
+              key={index}
+              style={[styles.fieldsText, styles.link]}
+              onPress={() => handleLinkPress(part)}
+          >
+            {part}
+          </Text>
+       : <Text key={index} style={styles.fieldsText}>{part}</Text>
+    );
+  };
+
   return <View style={{ flex: 1 }}>
     <ScrollView
         contentContainerStyle={[styles.scrollContainer, { padding: 16 }]}
@@ -37,22 +59,20 @@ const ProfileCard = () => {
           <Text style={styles.name}>
             {EXAMPLE_DETAILS[0]}
           </Text>
-          <Text style={{ fontSize: 16, color: "white", verticalAlign: "bottom" }}>
+          <Text style={{ fontSize: 16, color: "white", verticalAlign: "middle" }}>
             {EXAMPLE_DETAILS[1]}
           </Text>
         </View>
       </View>
-      {TEXT_FIELDS.map((field, index) => (
-          <>
+      {TEXT_FIELDS.slice(2, -1).map((field, index) => (
+          <View key={index}>
             <Text style={{ fontSize: 16, color: "white", marginHorizontal: 20 }}>
               {field}
             </Text>
             <View style={styles.fieldsContainer}>
-              <Text style={{ fontSize: 16, color: "white", flex: 2, padding: 8 }}>
-                {EXAMPLE_DETAILS[index + 2]}
-              </Text>
+              <LinkText text={EXAMPLE_DETAILS[index + 2]} />
             </View>
-          </>
+          </View>
       ))}
     </ScrollView>
   </View>
@@ -93,6 +113,16 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: Colors.primaryMuted,
   },
+  fieldsText: {
+    fontSize: 16,
+    color: "white",
+    flex: 2,
+    padding: 8
+  },
+  link: {
+    color: Colors.dark,
+    textDecorationLine: "underline",
+  }
 });
 
 export default ProfileCard;
