e26452d0171588858ff50de4c2a3783599ddbbe7
Ethan Hosier <ethanjhosier@gmail.com>
2024-05-29 17:59:43 +0100

    style: tweak create new project ui

--- app/(authenticated)/projects/_layout.tsx
+++ app/(authenticated)/projects/_layout.tsx
@@ -1,12 +1,39 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Colors from "@/constants/Colors";
 
 const Layout = () => {
+  const router = useRouter();
   return (
     <Stack>
       <Stack.Screen name="discover-projects" options={{ headerShown: false }} />
-      <Stack.Screen name="add-project" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="add-project"
+        options={{
+          presentation: "fullScreenModal",
+          title: "New Project",
+          headerLeft: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Text
+                style={{ color: Colors.primary, fontWeight: 500, fontSize: 16 }}
+              >
+                Cancel
+              </Text>
+            </TouchableOpacity>
+          ),
+          headerRight: () => (
+            <TouchableOpacity onPress={router.back}>
+              <Text
+                style={{ color: Colors.primary, fontWeight: 500, fontSize: 16 }}
+              >
+                Create
+              </Text>
+            </TouchableOpacity>
+          ),
+        }}
+      />
       <Stack.Screen
         name="[projectId]"
         options={{ presentation: "modal", headerShown: false }}
--- app/(authenticated)/projects/add-project.tsx
+++ app/(authenticated)/projects/add-project.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, TextInput} from "react-native";
-import {AntDesign} from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
-import {router} from "expo-router";
+import { router } from "expo-router";
 import { Image } from "expo-image";
-import {ScrollView} from "react-native-gesture-handler";
+import { ScrollView } from "react-native-gesture-handler";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
 const PROJECT_FIELDS = [
-    "Name",
-    "Description",
-    "Min Group Size",
-    "Max Group Size",
-    "Start Date",
-]
+  "Name",
+  "Description",
+  "Min Group Size",
+  "Max Group Size",
+  "Start Date",
+];
 
 const AddProjectPage = () => {
-  const [projectFields, setProjectFields] = React.useState({
+  const [projectFields, setProjectFields] = useState({
     img: "",
     name: "",
     description: "",
@@ -26,110 +34,134 @@ const AddProjectPage = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>Add New Project</Text>
-        <TouchableOpacity
-            style={{alignSelf: "flex-end"}}
-            onPress={router.back}
-        >
-          <AntDesign name="close" size={24} color="black"/>
-        </TouchableOpacity>
-      </View>
       <View style={styles.imgContainer}>
-        <TouchableOpacity onPress={() => alert("TODO")} style={{height: 200}}>
-          {(projectFields.img === "")
-              ? <Text
-                  style={{textAlign: "center", textAlignVertical: "center", fontSize: 24, color: Colors.gray}}
-              >
-                Upload an Image
-              </Text>
-              : <Image
-                  source={projectFields.img}
-                  style={styles.image}
-              />
-          }
+        <TouchableOpacity
+          onPress={() => alert("TODO")}
+          style={{
+            height: 200,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          {projectFields.img === "" ? (
+            <MaterialCommunityIcons
+              name="file-image-plus-outline"
+              size={64}
+              color={Colors.gray}
+            />
+          ) : (
+            <Image source={projectFields.img} style={styles.image} />
+          )}
         </TouchableOpacity>
       </View>
-      <View style={styles.fieldsContainer}>
+
+      <View style={[styles.fieldsContainer, { paddingBottom: 20 }]}>
         <ProjectField
-            label={PROJECT_FIELDS[0]}
-            value={projectFields["name"]}
-            onChange={(text: string) => setProjectFields({...projectFields, name: text})}
+          label={PROJECT_FIELDS[0]}
+          value={projectFields["name"]}
+          onChange={(text: string) =>
+            setProjectFields({ ...projectFields, name: text })
+          }
         />
         <ProjectField
-            label={PROJECT_FIELDS[1]}
-            value={projectFields["description"]}
-            style={{height: 100}}
-            onChange={(text: string) => setProjectFields({...projectFields, description: text})}
+          label={PROJECT_FIELDS[1]}
+          value={projectFields["description"]}
+          style={{ height: 100 }}
+          onChange={(text: string) =>
+            setProjectFields({ ...projectFields, description: text })
+          }
         />
-        <View style={{flexDirection: "row", gap: 8, width: "100%"}}>
-          <View style={{flex: 1}}>
+        <View style={{ flexDirection: "row", gap: 8, width: "100%" }}>
+          <View style={{ flex: 1 }}>
             <ProjectField
-                label={PROJECT_FIELDS[2]}
-                placeholder={'0'}
-                value={projectFields["minGroupSize"].toString()}
-                onChange={(text: string) => setProjectFields({...projectFields, minGroupSize: parseInt(text)})}
+              label={PROJECT_FIELDS[2]}
+              placeholder={"0"}
+              value={projectFields["minGroupSize"].toString()}
+              onChange={(text: string) =>
+                setProjectFields({
+                  ...projectFields,
+                  minGroupSize: parseInt(text),
+                })
+              }
             />
           </View>
-          <View style={{flex: 1}}>
+          <View style={{ flex: 1 }}>
             <ProjectField
-                placeholder={'0'}
-                label={PROJECT_FIELDS[3]}
-                value={projectFields["maxGroupSize"].toString()}
-                onChange={(text: string) => setProjectFields({...projectFields, maxGroupSize: parseInt(text)})}
+              placeholder={"0"}
+              label={PROJECT_FIELDS[3]}
+              value={projectFields["maxGroupSize"].toString()}
+              onChange={(text: string) =>
+                setProjectFields({
+                  ...projectFields,
+                  maxGroupSize: parseInt(text),
+                })
+              }
             />
           </View>
         </View>
-        <ProjectField // TODO: date picker
-            label={PROJECT_FIELDS[4]}
-            value={""}
-            placeholder={""}
-            onChange={(text: string) => {}}
-        />
-      </View>
-      <View style={styles.button}>
-        <TouchableOpacity onPress={() => alert("TODO")}>
-          <Text style={{color: "white", fontSize: 24, fontWeight: "700"}}>Submit</Text>
-        </TouchableOpacity>
+
+        <Text style={styles.fieldLabel}>Start Date & Time</Text>
+        <View style={{ flex: 1, flexDirection: "row", marginTop: 8, gap: 8 }}>
+          <RNDateTimePicker
+            onChange={() => {}}
+            value={new Date()}
+            style={{ marginLeft: -8 }}
+          />
+          <RNDateTimePicker
+            onChange={() => {}}
+            value={new Date()}
+            mode="time"
+            style={{ marginLeft: -10 }}
+          />
+        </View>
       </View>
     </ScrollView>
   );
-}
+};
 
-const ProjectField = ({label, placeholder = label, value, onChange, style}:
-    {
-      label: string,
-      placeholder?: string,
-      value: string,
-      height?: number,
-      onChange: (value: any) => void,
-      style?: any
-    }
-) => {
-  return (<>
-    <Text style={styles.fieldLabel}>{label}</Text>
-    <View style={styles.fieldContainer}>
+const ProjectField = ({
+  label,
+  placeholder = label,
+  value,
+  onChange,
+  style,
+}: {
+  label: string;
+  placeholder?: string;
+  value: string;
+  height?: number;
+  onChange: (value: any) => void;
+  style?: any;
+}) => {
+  return (
+    <>
+      <Text style={styles.fieldLabel}>{label}</Text>
       <TextInput
-          style={[styles.fieldInput, style]}
-          value={value}
-          onChangeText={onChange}
-          placeholder={placeholder}
-          placeholderTextColor={Colors.gray}
+        style={{
+          marginTop: 8,
+          width: "100%",
+          height: 40,
+          borderWidth: 1,
+          borderColor: Colors.lightGray,
+          padding: 8,
+          borderRadius: 12,
+        }}
+        placeholder={placeholder}
       />
-    </View>
-  </>);
-}
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24,
+    padding: 12,
     width: "100%",
     marginBottom: 50,
   },
   titleContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
     width: "100%",
     marginTop: 24,
     marginBottom: 32,
@@ -141,6 +173,7 @@ const styles = StyleSheet.create({
     verticalAlign: "bottom",
   },
   imgContainer: {
+    marginTop: 24,
     width: "100%",
     height: 200,
     borderRadius: 24,
@@ -165,22 +198,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     backgroundColor: "white",
     borderRadius: 20,
-    paddingTop: 4,
-    paddingBottom: 12,
-    marginBottom: 20,
-  },
-  fieldContainer: {
-    padding: 8,
-    borderRadius: 12,
-    borderWidth: 1,
-    borderColor: Colors.gray,
-    marginBottom: 8,
-    marginTop: 8,
   },
   fieldLabel: {
     fontSize: 16,
     fontWeight: "500",
-    marginTop: 16 },
+    marginTop: 16,
+  },
   fieldInput: {
     fontSize: 16,
     color: "black",
@@ -194,8 +217,8 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: Colors.primary,
     marginBottom: 50,
-    height: 50
-  }
+    height: 50,
+  },
 });
 
 export default AddProjectPage;
