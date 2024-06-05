e48371ffa8d0a202b1738c69fba0739d9d3d6047
Francois Poh <fp522@ic.ac.uk>
2024-06-05 15:51:39 +0100

    feat: image overlay for discover

--- components/projects/project-preview.tsx
+++ components/projects/project-preview.tsx
@@ -1,87 +1,90 @@
-import { StyleSheet, Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 import React from "react";
-import { Link } from "expo-router";
-import { ImageBackground } from "expo-image";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import { Ionicons } from "@expo/vector-icons";
+import {Link} from "expo-router";
+import {ImageBackground} from "expo-image";
+import {TouchableOpacity} from "react-native-gesture-handler";
+import {Ionicons} from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
-import { getMonthAbbreviation } from "@/utils/utils";
-import { BlurView } from "expo-blur";
-import { Project } from "@/utils/store/projects-store";
+import {getMonthAbbreviation} from "@/utils/utils";
+import {BlurView} from "expo-blur";
+import {Project} from "@/utils/store/projects-store";
 
 interface ProjectPreviewProps {
   project: Project;
 }
 
-const ProjectPreview: React.FC<ProjectPreviewProps> = ({ project }) => {
+const ProjectPreview: React.FC<ProjectPreviewProps> = ({project}) => {
   return (
-    <View style={{ marginLeft: 24 }}>
-      <Link
-        asChild
-        href={`/(authenticated)/projects/view-project/${project.projectId}`}
-      >
-        <TouchableOpacity onPress={() => console.log("yeahh")}>
-          <View style={{ width: 320, aspectRatio: 5 / 3 }}>
-            <ImageBackground
-              source={{ uri: project.image }}
-              style={styles.image}
-              imageStyle={{ borderRadius: 24 }}
-            >
-              <View
-                style={{
-                  borderRadius: 8,
-                  overflow: "hidden",
-                  width: 48,
-                  height: 48,
-                }}
+      <View style={{marginLeft: 24}}>
+        <Link
+            asChild
+            href={`/(authenticated)/projects/view-project/${project.projectId}`}
+        >
+          <TouchableOpacity onPress={() => console.log("yeahh")}>
+            <View style={{width: 320, aspectRatio: 5 / 3}}>
+              <ImageBackground
+                  source={{uri: project.image}}
+                  style={{borderRadius: 24, aspectRatio: 5 / 3, width: "100%"}}
+                  imageStyle={styles.image}
               >
-                <BlurView
-                  tint="extraLight"
-                  style={{
-                    backgroundColor: "rgba(255,255,255,0.3)",
-                    borderRadius: 8,
-                    height: 48,
-                    alignItems: "center",
-                    justifyContent: "center",
-                  }}
-                  intensity={20}
-                >
-                  <Text style={{ color: "#fff", fontSize: 12 }}>
-                    {getMonthAbbreviation(project.startDateTime)}
-                  </Text>
-                  <Text
-                    style={{
-                      color: "#fff",
-                      textAlign: "center",
-                      fontWeight: "bold",
-                      fontSize: 18,
-                    }}
+                <View style={styles.imageOverlay}>
+                  <View
+                      style={{
+                        margin: 16,
+                        borderRadius: 8,
+                        overflow: "hidden",
+                        width: 48,
+                        height: 48,
+                      }}
                   >
-                    {project.startDateTime.getDate()}
-                  </Text>
-                </BlurView>
-              </View>
-            </ImageBackground>
-          </View>
-          <Text style={{ marginTop: 8, fontWeight: "700", fontSize: 16 }}>
-            {project.name}
-          </Text>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Ionicons name="people-outline" size={24} color={Colors.gray} />
-            <Text
-              style={{
-                color: Colors.gray,
-                fontWeight: "600",
-                marginLeft: 5,
-                marginTop: 1,
-              }}
-            >
-              {project.minGroupSize}-{project.maxGroupSize} members
+                    <BlurView
+                        tint="extraLight"
+                        style={{
+                          backgroundColor: "rgba(255,255,255,0.3)",
+                          borderRadius: 8,
+                          height: 48,
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                        intensity={20}
+                    >
+                      <Text style={{color: "#fff", fontSize: 12}}>
+                        {getMonthAbbreviation(project.startDateTime)}
+                      </Text>
+                      <Text
+                          style={{
+                            color: "#fff",
+                            textAlign: "center",
+                            fontWeight: "bold",
+                            fontSize: 18,
+                          }}
+                      >
+                        {project.startDateTime.getDate()}
+                      </Text>
+                    </BlurView>
+                  </View>
+                </View>
+              </ImageBackground>
+            </View>
+            <Text style={{marginTop: 8, fontWeight: "700", fontSize: 16}}>
+              {project.name}
             </Text>
-          </View>
-        </TouchableOpacity>
-      </Link>
-    </View>
+            <View style={{flexDirection: "row", alignItems: "center"}}>
+              <Ionicons name="people-outline" size={24} color={Colors.gray}/>
+              <Text
+                  style={{
+                    color: Colors.gray,
+                    fontWeight: "600",
+                    marginLeft: 5,
+                    marginTop: 1,
+                  }}
+              >
+                {project.minGroupSize}-{project.maxGroupSize} members
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </Link>
+      </View>
   );
 };
 
@@ -97,4 +100,10 @@ const styles = StyleSheet.create({
     borderRadius: 24,
     padding: 16,
   },
+  imageOverlay: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: 'rgba(0, 0, 0, 0.02)',
+    borderRadius: 24
+}
 });
