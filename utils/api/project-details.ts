9fdded2276f4e249f4131c9810244efca3955f31
Henry Yu <henrytsyu@gmail.com>
2024-06-05 15:54:21 +0100

    feat: fetch groups in specific project

--- utils/api/project-details.ts
+++ utils/api/project-details.ts
@@ -9,6 +9,12 @@ export type Project = {
   start_date_time: string;
 };
 
+export type Group = {
+  group_id: string;
+  description: string;
+  members: string[];
+};
+
 export const getProjectDetails: (
   projectId: string
 ) => Promise<DRPResponse<Project>> = async (projectId) => {
@@ -25,10 +31,36 @@ export const getProjectDetails: (
 
 export const getProjectGroups: (
   projectId: string
-) => Promise<DRPResponse<null>> = async (projectId) => {
-  const {} = await supabase
-    .from("projects")
-    .select("")
+) => Promise<DRPResponse<Group[]>> = async (projectId) => {
+  const { data: groups, error: groupIdError } = await supabase
+    .from("groups")
+    .select("group_id, description")
     .eq("project_id", projectId);
-  return { data: null, error: null };
+  if (groupIdError) return { data: null, error: groupIdError };
+
+  const groupIds = groups.map((group) => group.group_id);
+
+  const memberIds = new Map<string, string[]>();
+  const errors = await Promise.all(
+    groupIds.map(async (groupId) => {
+      const { data: memberIdsInGroup, error: memberIdError } = await supabase
+        .from("group_members")
+        .select("user_id")
+        .eq("group_id", groupId);
+      if (memberIdError) return memberIdError;
+      memberIds.set(
+        groupId,
+        memberIdsInGroup.map((e) => e.user_id)
+      );
+      return null;
+    })
+  );
+  for (const error of errors) {
+    if (error) return { data: null, error };
+  }
+  const data = groups.map((group) => ({
+    ...group,
+    members: memberIds.get(group.group_id)!,
+  }));
+  return { data, error: null };
 };
