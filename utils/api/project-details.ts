446557fe1c37e6fe93b2f9b4babad8c214a32dba
Henry Yu <henrytsyu@gmail.com>
2024-06-06 02:06:16 +0100

    Revert "fix: enforce single profile per member"
    This reverts commit 5373896d49885b3b384efd374e402e189fcf711a.

--- utils/api/project-details.ts
+++ utils/api/project-details.ts
@@ -50,7 +50,7 @@ export const getProjectGroups: (
       group_id: group.group_id,
       description: group.description,
       members: group.group_members.map((member, i) => {
-        const { user_id, ...profileWithoutUserId } = member.profiles[0];
+        const { user_id, ...profileWithoutUserId } = member.profiles;
         console.log(member.profiles);
         return {
           ...profileWithoutUserId,
