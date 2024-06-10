62a7cc9f096c47ab6a5055bdfeae5bb69b1817c1
Francois Poh <fp522@ic.ac.uk>
2024-06-10 16:37:05 +0100

    feat: view profiles and refresh for interested

--- app/(modals)/group/[groupId]/view-interested.tsx
+++ app/(modals)/group/[groupId]/view-interested.tsx
@@ -1,18 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import { useLocalSearchParams } from "expo-router";
+import {Link, useLocalSearchParams} from "expo-router";
 import Colors from "@/constants/Colors";
 import { Image } from "expo-image";
-import { ScrollView } from "react-native-gesture-handler";
+import {RefreshControl, ScrollView} from "react-native-gesture-handler";
 import { Group } from "@/utils/api/project-details";
+import {useUserIdStore} from "@/utils/store/user-id-store";
+import {getGroupRequests} from "@/utils/api/groups";
 
 const ViewInterested = () => {
-  const { interested } = useLocalSearchParams();
-  const groups = JSON.parse(interested as string) as Group[];
+
+
+  const userId = useUserIdStore((state) => state.userId);
+  const [loading, setLoading] = useState(false);
+  const { groupId, interested } = useLocalSearchParams();
+  const [groups, setGroups] = useState(JSON.parse(interested as string) as Group[]);
+
+  const refreshGroups = async () => {
+    if (!groupId || !userId) return;
+    getGroupRequests(groupId as string).then((res) => {
+      if (res.error) {
+        console.error(res.error);
+        return;
+      }
+      setGroups(res.data);
+    });
+  }
+
+  const refresh = async () => {
+    setLoading(true);
+    await Promise.all([refreshGroups()]);
+    setLoading(false);
+  }
 
   return (
     <View style={[styles.container]}>
       <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={loading} onRefresh={refresh} />
+        }
         style={{ paddingTop: 24 }}
         contentContainerStyle={{ gap: 16 }}
       >
@@ -50,12 +76,14 @@ const ViewInterested = () => {
               contentContainerStyle={{ paddingLeft: 16, gap: 16 }}
             >
               {group.members.map((member, id) => (
-                <TouchableOpacity key={id} style={{ marginRight: 12 }}>
-                  <Image source={member.imageUrl} style={styles.image} />
-                  <Text style={[styles.text, { marginTop: 8 }]}>
-                    {member.full_name.split(" ")[0]}
-                  </Text>
-                </TouchableOpacity>
+                  <Link href={`/(modals)/view-profile/${member.id}`} asChild key={id}>
+                    <TouchableOpacity key={id} style={{ marginRight: 12 }}>
+                      <Image source={member.imageUrl} style={styles.image} />
+                      <Text style={[styles.text, { marginTop: 8 }]}>
+                        {member.full_name.split(" ")[0]}
+                      </Text>
+                    </TouchableOpacity>
+                  </Link>
               ))}
             </ScrollView>
             {index < groups.length - 1 && (
