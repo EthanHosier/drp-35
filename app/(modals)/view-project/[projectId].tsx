9f4b1ea1a5dc04c28475fbf8c93cefbdf221edf8
Francois Poh <fp522@ic.ac.uk>
2024-06-12 13:39:12 +0100

    feat: do not display groups that are too large in swipe

--- app/(modals)/view-project/[projectId].tsx
+++ app/(modals)/view-project/[projectId].tsx
@@ -23,7 +23,12 @@ import { useFilterStore } from "@/utils/store/filter-store";
 import LottieView from "lottie-react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { defaultStyles } from "@/constants/DefaultStyles";
-import {acceptRequestToJoinGroup, isMatch, requestToJoinGroup} from "@/utils/api/groups";
+import {
+  acceptRequestToJoinGroup,
+  getGroupById,
+  isMatch,
+  requestToJoinGroup
+} from "@/utils/api/groups";
 import {useUserIdStore} from "@/utils/store/user-id-store";
 import {supabase} from "@/utils/supabase";
 
@@ -92,6 +97,7 @@ const GroupsTab = () => {
   const projectId = useLocalSearchParams().projectId as string;
   const userId = useUserIdStore((state) => state.userId);
   const [groupId, setGroupId] = useState<string | null>(null);
+  const [membersNeeded, setMembersNeeded] = useState<number>(0);
 
   const getGroupId = async () => {
     const { data, error } = await supabase
@@ -108,6 +114,19 @@ const GroupsTab = () => {
     .eq("groups.project_id", projectId)
     .single();
     if (!error && data.groups) setGroupId(data.groups!.group_id);
+  }
+
+  const getMembersNeeded = async () => {
+    const { data: project, error: maxError } = await supabase
+      .from("projects")
+      .select("max_group_size")
+      .eq("project_id", projectId)
+      .single();
+    if (maxError) return console.error(maxError);
+
+    await getGroupById(groupId as string).then((res) => {
+      if (res.data) setMembersNeeded(project.max_group_size - res.data?.members.length)
+    });
   }
 
   useEffect(() => {
@@ -211,6 +230,7 @@ const GroupsTab = () => {
                 ? projectGroups.filter((group) => {
                     return (
                       (group.group_id !== groupId) &&
+                      (group.members.length <= membersNeeded) &&
                       (numMembers <= 0 ||
                         group.members.length === numMembers) &&
                       (languages.length <= 0 ||
